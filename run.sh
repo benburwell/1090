ce6c853650b491fbd21170b29ad7f70f578d6829
Ben Burwell <ben.burwell@trifecta.com>
2016-11-15 23:58:32 -0500

    Update build & run script

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-javac **/*.java
+mkdir -p out/production/1090
+javac -d out/production/1090 $(find src -name '*.java')
 cd out/production/1090
 jar -cvfe planes.jar com.benburwell.planes.gui.Main1090 **
 java -jar planes.jar
